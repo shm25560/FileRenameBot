5395e2be7f84935b0cd90602903733aacec4c296
Shahab Error <71049566+shm25560@users.noreply.github.com>
2021-03-01 10:27:45 -0800

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -10,7 +10,7 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH")
     # Get these values from my.telegram.org
     # Array to store users who are authorized to use the bot
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", ["501197185", "1321184681", "1067661613"]).split())
     # Banned Unwanted Members..
     BANNED_USERS = []
     # the download location, where the HTTP Server runs
